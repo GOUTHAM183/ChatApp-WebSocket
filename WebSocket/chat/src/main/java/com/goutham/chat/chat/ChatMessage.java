0f833d9e9e3521a83e5625a5bee54026878ab128
GOUTHAM183 <gouthamlucky68@gmail.com>
2024-03-28 08:32:38 -0500

    Initial commit

---
+++ WebSocket/chat/src/main/java/com/goutham/chat/chat/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.goutham.chat.chat;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+
+}
---
+++ WebSocket/chat/src/main/java/com/goutham/chat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.goutham.chat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
